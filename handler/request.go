15bfe92d964c0783799045e94ebba4f334229441
Lucas Candido <avnserver@gmail.com>
2023-12-06 03:40:59 -0300

    feat:  add update handler logic

--- handler/request.go
+++ handler/request.go
@@ -2,48 +2,62 @@ package handler
 
 import "fmt"
 
-
 func errParamsIsRequired(name, typ string) error {
-  return fmt.Errorf("Param: %s (Type: %s) is required!", name, typ)
+	return fmt.Errorf("Param: %s (Type: %s) is required!", name, typ)
 }
 
 // CreateOpening
 
 type CreateOpeningRequest struct {
-	Role      string    `json:"role"`
-	Company   string    `json:"company"`
-	Location  string    `json:"location"`
-	Remote    *bool      `json:"remote"`
-	Link      string    `json:"link"`
-	Salary    int64     `json:"salary"`
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
 }
 
 func (r *CreateOpeningRequest) validate() error {
 
+	if r.Role == "" || r.Company == "" || r.Location == "" || r.Remote == nil || r.Salary <= 0 {
+		return fmt.Errorf("request body is empty or malformed")
+	}
 
+	if r.Role == "" {
+		return errParamsIsRequired("role", "string")
+	}
 
-  if r.Role == "" || r.Company == "" || r.Location == "" || r.Remote == nil || r.Salary <= 0 {
-    return fmt.Errorf("request body is empty or malformed")
+	if r.Company == "" {
+		return errParamsIsRequired("company", "string")
+	}
+	if r.Location == "" {
+		return errParamsIsRequired("location", "string")
+	}
+	if r.Remote == nil {
+		return errParamsIsRequired("remote", "bool")
+	}
+	if r.Link == "" {
+		return errParamsIsRequired("link", "string")
+	}
+	if r.Salary <= 0 {
+		return errParamsIsRequired("salary", "int64")
+	}
+	return nil
 }
- 
-  if r.Role == "" {
-    return errParamsIsRequired("role", "string")
-  }
 
-  if r.Company == "" {
-    return errParamsIsRequired("company", "string")
-  }
-  if r.Location == "" {
-    return errParamsIsRequired("location", "string")
-  }
-  if r.Remote == nil {
-    return errParamsIsRequired("remote", "bool")
-  } 
-  if r.Link == "" {
-    return errParamsIsRequired("link", "string")
-  }
-  if r.Salary <= 0 {
-    return errParamsIsRequired("salary","int64")
-  }
-  return nil
+type UpdateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *UpdateOpeningRequest) Validate() error {
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Remote != nil || r.Link != "" || r.Salary > 0 {
+		return nil
+	}
+	// If none of the fields were provided, return falsy
+	return fmt.Errorf("at least one valid field must be provided")
 }
